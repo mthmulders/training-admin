267446d8e3fa8ba2e3579a0310a038865ec25424
Maarten Mulders <m.th.mulders@xs4all.nl>
2025-03-11 21:06:13 +0100

    feat: Proper error message when using duplicate key

--- src/main/kotlin/it/mulders/talks/trainingadmin/shared/ExceptionHandlingController.kt
+++ src/main/kotlin/it/mulders/talks/trainingadmin/shared/ExceptionHandlingController.kt
@@ -1,6 +1,7 @@
 package it.mulders.talks.trainingadmin.shared
 
 import it.mulders.talks.trainingadmin.courses.CourseNotFoundException
+import org.springframework.dao.DuplicateKeyException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.servlet.ModelAndView
@@ -11,5 +12,10 @@ class ExceptionHandlingController {
     @ExceptionHandler(*[CourseNotFoundException::class])
     fun courseNotFound(exception: Exception): ModelAndView = ModelAndView("shared/error").apply {
         addObject("message", exception.message)
+    }
+
+    @ExceptionHandler(DuplicateKeyException::class)
+    fun duplicateKey(exception: DuplicateKeyException): ModelAndView = ModelAndView("shared/error").apply {
+        addObject("message", "That identifier is already in use")
     }
 }
