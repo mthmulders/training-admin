6324c2d1ba63aa62a23f41a3aa26e1bdf6b9380a
Maarten Mulders <mthmulders@users.noreply.github.com>
2025-03-11 16:39:47 +0100

    Add duration field to Course entity
    Fixes #5
    
    Add a 'duration' field to the Course entity.
    
    * Modify `Course.kt` to include a new `duration` field of type `Int` with a default value of 0.
    * Add a new migration script `V2__Add_duration_to_Course.sql` to add the `duration` column to the `course` table with a default value of 0 for existing records.
    * Update `edit.html` to include an input field for the `duration` attribute within the course form.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/mthmulders/training-admin/issues/5?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/main/kotlin/it/mulders/talks/trainingadmin/courses/Course.kt
+++ src/main/kotlin/it/mulders/talks/trainingadmin/courses/Course.kt
@@ -12,9 +12,10 @@ data class Course(
     @field:Id  val id: UUID?,
     @field:NotNull @field:NotBlank val courseId: String,
     @field:NotNull @field:NotBlank val name: String,
+    @field:NotNull val duration: Int = 0
 ) {
     @PersistenceCreator
-    constructor(id: UUID?) : this(id, "", "")
+    constructor(id: UUID?) : this(id, "", "", 0)
 
     constructor() : this(null)
 }
